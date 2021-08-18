16126ed183183d661de39c1177cbedfa85ebf741
Hammad <hammadrahman162@gmail.com>
2021-08-18 21:30:43 +0600

    updated with fully responsive

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,26 +1,34 @@
-// Slick slider
+// owl carousel
 $(document).ready(function() {
-	$('.customer-logos').slick({
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 1500,
-		arrows: false,
-		dots: false,
-		pauseOnHover: false,
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 4
-				}
-			},
-			{
-				breakpoint: 520,
-				settings: {
-					slidesToShow: 3
-				}
-			}
-		]
-	});
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        smartSpeed: 850,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        nav: true,
+        loop: true,
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
 });
+
+// Hamberger Navigation
+const Hamberger = document.querySelector('.navigation'),
+    navtogglerbtn = document.querySelector('.nav-toggler');
+navtogglerbtn.addEventListener('click', popuptoggler);
+
+function popuptoggler() {
+    navtogglerbtn.classList.toggle('open');
+    $('.navigation').slideToggle(300);
+}
